6142c2adf83603f60722f06fc70e46c0dd6d2014
Jayam pandit <94307580+jayam30@users.noreply.github.com>
2025-04-24 17:50:29 +0530

    Update _app.tsx

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,9 +1,23 @@
-import { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { queryClient as importedQueryClient } from '@/lib/queries';
+// import { AppProps } from 'next/app';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { queryClient as importedQueryClient } from '@/lib/queries';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const queryClient = new QueryClient();
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// const queryClient = new QueryClient();
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return (
+//     <QueryClientProvider client={importedQueryClient}>
+//       <Component {...pageProps} />
+//     </QueryClientProvider>
+//   );
+// }
+
+// export default MyApp;
+
+import { AppProps } from 'next/app';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient as importedQueryClient } from '@/lib/queries';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,3 +28,4 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
+
