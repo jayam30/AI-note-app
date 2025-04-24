0c7031fba82f7e3d12555c9498d20773d71caf1e
Jayam pandit <94307580+jayam30@users.noreply.github.com>
2025-04-24 18:02:46 +0530

    Update _app.tsx

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,20 +1,3 @@
-// import { AppProps } from 'next/app';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { queryClient as importedQueryClient } from '@/lib/queries';
-
-// // eslint-disable-next-line @typescript-eslint/no-unused-vars
-// const queryClient = new QueryClient();
-
-// function MyApp({ Component, pageProps }: AppProps) {
-//   return (
-//     <QueryClientProvider client={importedQueryClient}>
-//       <Component {...pageProps} />
-//     </QueryClientProvider>
-//   );
-// }
-
-// export default MyApp;
-
 import { AppProps } from 'next/app';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient as importedQueryClient } from '@/lib/queries';
@@ -28,4 +11,3 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
-
